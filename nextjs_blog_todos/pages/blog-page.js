558bc5217e5c9ae696048ee319229b4ab5999c85
tachoco <vanilla.airi@gmail.com>
2023-05-07 12:43:35 +0900

    4-38 main page の作成まで

---
+++ nextjs_blog_todos/pages/blog-page.js
@@ -0,0 +1,20 @@
+import Layout from "../components/Layout"
+import Link from "next/link"
+
+export default function BlogPage() {
+  return(
+    <Layout title="Blog page">
+      <Link href="/main-page">
+        <div className="flex cursor-pointer mt-12">
+          <svg 
+            className="w-6 h-6 mr-3"
+            fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"></path>
+          </svg>
+          <span>Back to main page</span>
+        </div>
+      </Link>
+    </Layout>
+  )
+}
+
---
+++ nextjs_blog_todos/pages/main-page.js
@@ -0,0 +1,38 @@
+import Cookie from "universal-cookie";
+import { useRouter } from "next/router";
+import Layout from "../components/Layout";
+import Link from "next/link";
+
+const cookie = new Cookie();
+
+export default function MainPage() {
+  const router = useRouter();
+
+  const logout = () => {
+    cookie.remove("access_token");
+    router.push("/")
+  }
+
+  return (
+    <Layout title="Main page">
+      <div className="mb-10">
+        <Link href="/blog-page">
+          <a className="bg-indigo-500 mr-8 hover:bg-indigo-600 text-white px-4 py-12 rounded">
+            Visit Blog by SSG + ISR
+          </a>
+        </Link>
+        <Link href="/task-page">
+          <a className="bg-gray-500 ml-8 hover:bg-gray-600 text-white px-4 py-12 rounded">
+            Visit Task by SSG + ISR
+          </a>
+        </Link>
+      </div>
+      <svg 
+        onClick={logout}
+        className="mt-10 cursor-pointer w-6 h-6"
+        fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"></path>
+      </svg>
+    </Layout>
+  )
+}
